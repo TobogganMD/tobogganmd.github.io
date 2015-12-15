4969e329a4e49becf34b2cfafee82e9c82f6c5a9
Gunnar Bondsater <gunnar.bondsater@gmail.com>
2015-12-15 09:54:24 +0100

    initial commit!

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+var myHeading = document.querySelector('h1');
+var myButton = document.querySelector('button');
+
+function setName() {
+	var myName = prompt('What\'s your name?');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Hello, ' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+	setName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Hello, ' + storedName;
+}
+
+myButton.onClick = function() {
+	setName();
+}
